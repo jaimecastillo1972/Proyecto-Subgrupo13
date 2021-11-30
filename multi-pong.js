65ccec9a67205ca5f81eb944468f0fdb0cb20936
Jonathan Oriel Rodríguez Chacón <jon_2611@outlook.com>
2021-11-30 01:10:04 -0500

    primeras lineas de codigo

--- multi-pong.js
+++ multi-pong.js
@@ -1,1 +1,40 @@
-console.log('hello world')
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+@SuppressWarnings("serial")
+public class Pingpong extends JPanel {
+    Ball ball = new Ball(this);
+    Racquet racquet = new Racquet(this);
+    int speed = 1;
+
+    private int getScore() {
+        return speed - 1;
+    }
+
+    public Pingpong() {
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                racquet.keyReleased(e);
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                racquet.keyPressed(e);
+            }
+        });
+        setFocusable(true);
+    }
+
+    private void move() {
+        ball.move();
+        racquet.move();
+    }
+
+    
